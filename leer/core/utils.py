e988206c24545e169bf5ecf0aca7b631bb221966
WTRMQDev <TetsuwanAtomu@tuta.io>
2018-12-07 14:45:20 +0300

    Mover _hash (sha256) to core.utils

--- leer/core/utils.py
+++ leer/core/utils.py
@@ -72,3 +72,10 @@ class ObliviousDictionary:
       else:
          break #trigger_time_list is sorted
     self.trigger_time_list=self.trigger_time_list[n:]
+
+
+import hashlib
+def sha256(data):
+        m=hashlib.sha256()
+        m.update(bytes(data))
+        return m.digest()
