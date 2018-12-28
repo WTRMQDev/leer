725db8ddcd7bd7d32bef52f01be9e0cf4c719b2b
WTRMQDev <TetsuwanAtomu@tuta.io>
2018-12-28 10:19:35 +0300

    Add argumetns to lookup stubs

--- leer/core/storage/lookup_utils.py
+++ leer/core/storage/lookup_utils.py
@@ -1,5 +1,6 @@
-def excess_lookup():
+#All arguments are made keyword arguments for easier partial usage
+def excess_lookup( pubkey=None, _hash=None, rtx=None, tx=None):
   pass
 
-def output_lookup():
+def output_lookup(commitment=None, rtx=None, tx=None):
   pass
