61af0f31811335d07fbcf6afbaedea4c2c969d6f
WTRMQDev <TetsuwanAtomu@tuta.io>
2018-12-29 08:58:32 +0300

    Check lookup arguments and cast point to corresponding point-like objects

--- leer/core/storage/lookup_utils.py
+++ leer/core/storage/lookup_utils.py
@@ -1,6 +1,14 @@
 #All arguments are made keyword arguments for easier partial usage
 def excess_lookup( pubkey=None, _hash=None, rtx=None, tx=None):
-  pass
+  assert pubkey
+  assert _hash
+  assert rtx
+  assert tx
+  pk = pubkey.to_pubkey()
 
 def output_lookup(commitment=None, rtx=None, tx=None):
-  pass
+  assert commitment
+  assert rtx
+  assert tx
+  commitment = commitment.to_pedersen_commitment()
+
