67bfdbf90978e5dd0a321dcaab5c032e97448661
WTRMQDev <TetsuwanAtomu@tuta.io>
2019-01-21 16:09:09 +0300

    Modify mining_canary hash to allow getwork protocol

--- leer/core/hash/mining_canary.py
+++ leer/core/hash/mining_canary.py
@@ -2,9 +2,14 @@ from secp256k1_zkp import PrivateKey
 from hashlib import sha256
 
 def mining_canary_hash(_bytes):
-    m = sha256()
-    m.update(_bytes)
-    p = PrivateKey(privkey=m.digest(), raw=True)
+    assert len(_bytes)>16
+    seed1 = _bytes[:-16]
+    m1 = sha256()
+    m1.update(seed1)
+    seed2 = m1.digest()
     m2 = sha256()
-    m2.update(p.pubkey.serialize()[1:])
-    return m2.digest()
+    m2.update(seed2+_bytes[-16:])    
+    p = PrivateKey(privkey=m2.digest(), raw=True)
+    m3 = sha256()
+    m3.update(p.pubkey.serialize()[1:])
+    return m3.digest()
