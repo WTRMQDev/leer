3a22196a0d22ea58f8faeec07a034014084aeb78
WTRMQDev <TetsuwanAtomu@tuta.io>
2019-01-26 23:13:28 +0300

    Modify mining_canary: isolate first round hashing data from second one

--- leer/core/hash/mining_canary.py
+++ leer/core/hash/mining_canary.py
@@ -8,11 +8,15 @@ def partial_hash(_bytes):
     m1.update(seed1)
     return m1.digest()
 
-def mining_canary_hash(_bytes):
-    seed2 = partial_hash(_bytes)
+def mining_canary_hash_part(_bytes):
     m2 = sha256()
-    m2.update(seed2+_bytes[-16:])    
+    m2.update(_bytes)    
     p = PrivateKey(privkey=m2.digest(), raw=True)
     m3 = sha256()
     m3.update(p.pubkey.serialize()[1:])
     return m3.digest()
+
+def mining_canary_hash(_bytes):
+    ph = partial_hash(_bytes)
+    return mining_canary_hash_part(ph)
+
