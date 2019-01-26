dc466372ae1b3a664d1fafbcd9e6de2829538c52
WTRMQDev <TetsuwanAtomu@tuta.io>
2019-01-26 15:41:52 +0300

    Separate partial_hash and mining_canary_hash

--- leer/core/hash/mining_canary.py
+++ leer/core/hash/mining_canary.py
@@ -1,12 +1,15 @@
 from secp256k1_zkp import PrivateKey
 from hashlib import sha256
 
-def mining_canary_hash(_bytes):
+def partial_hash(_bytes):
     assert len(_bytes)>16
     seed1 = _bytes[:-16]
     m1 = sha256()
     m1.update(seed1)
-    seed2 = m1.digest()
+    return m1.digest()
+
+def mining_canary_hash(_bytes):
+    seed2 = partial_hash(_bytes)
     m2 = sha256()
     m2.update(seed2+_bytes[-16:])    
     p = PrivateKey(privkey=m2.digest(), raw=True)
