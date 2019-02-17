8c1900c1fcc2bccf04cbb1f276eccb68fdb63356
WTRMQDev <TetsuwanAtomu@tuta.io>
2019-02-17 23:02:33 +0300

    Fix typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="leer",
-    version="0.1.0",
+    version="0.1.1",
     author="Evil Morty, Crez Khansick, Sark Czenchi",
     author_email="TetsuwanAtomu@tuta.io", 
     description="Leer cryptocurrency (alpha testing)",
