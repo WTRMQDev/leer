389c10d914f516472b631731c96c0639747dee67
WTRMQDev <TetsuwanAtomu@tuta.io>
2019-02-21 12:40:27 +0300

    Cache progpow_hashing

--- leer/core/hash/progpow.py
+++ leer/core/hash/progpow.py
@@ -1,15 +1,19 @@
 from hashlib import sha256
 import progpow
+import functools
+
+
 handler = progpow.ProgPowHandler(max_contexts_num=3)
 _progpow_hash = handler.hash
 
+@functools.lru_cache(maxsize=256)
 def partial_hash(_bytes):
     seed1 = _bytes
     m1 = sha256()
     m1.update(seed1)
     return m1.digest()
 
-
+@functools.lru_cache(maxsize=256)
 def progpow_hash(header_height, serialized_header_without_nonce, nonce_bytes):
     ph = partial_hash(serialized_header_without_nonce)
     return _progpow_hash(header_height, ph, int.from_bytes(nonce_bytes, "big"))
