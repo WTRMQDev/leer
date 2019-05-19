77dbbed7a4a1ac80516713acb17e444ff2f125f4
WTRMQDev <TetsuwanAtomu@tuta.io>
2019-05-19 12:36:46 +0300

    Add get_new_address func to CoreContext

--- leer/core/core_operations/core_context.py
+++ leer/core/core_operations/core_context.py
@@ -5,7 +5,7 @@ class CoreContext:
     It contains storage_space, core logger, nodes list, send_notification and send_message interfaces.
   """
 
-  def __init__(self, storage_space, logger, nodes, send_notification, send_message, config):
+  def __init__(self, storage_space, logger, nodes, send_notification, send_message, get_new_address, config):
     self.storage_space = storage_space
     self.logger = logger
     self.nodes = nodes
@@ -13,4 +13,5 @@ class CoreContext:
     self.send_to = send_message
     self.send_to_nm = partial(self.send_to, "NetworkManager")
     self.config = config
+    self.get_new_address = get_new_address
 
