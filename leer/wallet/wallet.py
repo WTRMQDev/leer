c8cba49a147e82c3dab0dbe5f49c9796fa23bfa4
WTRMQDev <TetsuwanAtomu@tuta.io>
2019-06-09 22:49:23 +0300

    Use KeyDB instead of KeyManager (untested)

--- leer/wallet/wallet.py
+++ leer/wallet/wallet.py
@@ -1,5 +1,6 @@
 from time import time, sleep
-from leer.wallet.key_manager import KeyManagerClass
+from leer.wallet.key_db import KeyDB
+from leer.wallet.key_db_migrations import apply_migrations
 
 from leer.core.lubbadubdub.ioput import IOput
 from leer.core.lubbadubdub.address import Address
@@ -75,7 +76,9 @@ def wallet(syncer, config):
 
   message_queue = syncer.queues['Wallet']
   _path = config['location']['wallet']
-  km = KeyManagerClass(path=_path)
+  km = KeyDB(path=_path)
+  with km.open() as cursor:
+    apply_migrations(cursor)
   notify('last wallet update', time())
   while True:
     sleep(0.01)
@@ -93,25 +96,25 @@ def wallet(syncer, config):
         tx.deserialize(message['tx'], rtx=None, skip_verification=True) #skip_verification allows us to not provide rtx
         block_height = message['height']
         last_time_updated = None
-        with km.disc_txn(write = True) as w_txn:
+        with km.open() as cursor:
           for index in tx.inputs:
             #Note it is not check whether output is unspent or not, we check that output is marked as our and unspent in our wallet
-            if km.is_unspent(index, r_txn=w_txn):
-              km.spend_output(index, block_height, w_txn=w_txn)
+            if km.is_unspent(index, cursor):
+              km.spend_output(index, block_height, cursor)
               last_time_updated = time()
           for _o in tx.outputs:
-            if km.is_owned_pubkey(_o.address.pubkey.serialize(), r_txn=w_txn):
-              km.add_output(_o, block_height, w_txn=w_txn)
+            if km.is_owned_pubkey(_o.address.pubkey.serialize(), cursor):
+              km.add_output(_o, block_height, cursor)
               last_time_updated = time()
-            if km.is_saved(_o, r_txn=w_txn):
-              km.register_processed_output(_o.serialized_index, block_height, w_txn=w_txn)
+            if km.is_saved(_o, cursor):
+              km.register_processed_output(_o.serialized_index, block_height, cursor)
         if last_time_updated:
           notify('last wallet update', last_time_updated)
       if message['action']=="process rollback":
         rollback = message['rollback_object']
         block_height = message['block_height']
-        with km.disc_txn(write = True) as w_txn:
-          km.rollback(block_height, w_txn=w_txn)
+        with km.open() as cursor:
+          km.rollback(block_height, cursor)
         last_time_updated = time()
         notify('last wallet update', last_time_updated)
       if message['action']=="process indexed outputs": #during private key import correspondent outputs will be processed again
@@ -122,16 +125,16 @@ def wallet(syncer, config):
         response = {"id": message["id"], "result": address.to_text()}
         syncer.queues[message['sender']].put(response)
       if message['action']=="give new address":
-        with km.disc_txn(write = True) as w_txn:
-          address = km.new_address(w_txn)
+        with km.open() as cursor:
+          address = km.new_address(cursor)
         response = {"id": message["id"], "result": address.serialize()}
         syncer.queues[message['sender']].put(response)
       if message['action']=="get confirmed balance stats":
         response = {"id": message["id"]}
         try:
           height = get_height()
-          with km.disc_txn(write = False) as r_txn:
-            stats = km.get_confirmed_balance_stats(height, r_txn)
+          with km.open() as cursor:
+            stats = km.get_confirmed_balance_stats(height, cursor)
           response["result"] = stats
         except KeyError:
           response["result"] = "error: core_loop didn't set height yet"
@@ -142,8 +145,8 @@ def wallet(syncer, config):
         response = {"id": message["id"]}
         try:
           height = get_height()
-          with km.disc_txn(write = True) as r_txn:
-            stats = km.get_confirmed_balance_list(height, r_txn)
+          with km.open() as cursor:
+            stats = km.get_confirmed_balance_list(height, cursor)
           response["result"] = stats
         except KeyError:
           response["result"] = "error: core_loop didn't set height yet"
@@ -154,8 +157,8 @@ def wallet(syncer, config):
         taddress = message["address"]
         a = Address()
         a.from_text(taddress)        
-        with km.disc_txn(write = False) as r_txn:
-            priv = km.priv_by_address(a, r_txn)
+        with km.open() as cursor:
+            priv = km.priv_by_address(a, cursor)
         response = {"id": message["id"], "result": priv.private_key}
         syncer.queues[message['sender']].put(response)
       if message['action']=="take private key":
@@ -165,8 +168,8 @@ def wallet(syncer, config):
       if message['action']=="give last transactions info":
         response = {"id": message["id"]}
         num = int(message["num"])
-        with km.disc_txn(write = False) as r_txn:
-          response["result"] = km.give_transactions(num, r_txn)
+        with km.open() as cursor:
+          response["result"] = km.give_transactions(num, cursor)
         syncer.queues[message['sender']].put(response) 
         continue
       if message['action']=="generate tx":
@@ -187,8 +190,8 @@ def wallet(syncer, config):
            response["error"] = str(e)
            syncer.queues[message['sender']].put(response)
            continue
-        with km.disc_txn(write = True) as w_txn:
-          _list = km.get_confirmed_balance_list(current_height, r_txn=w_txn)
+        with km.open() as cursor:
+          _list = km.get_confirmed_balance_list(current_height, cursor)
           list_to_spend = []
           summ = 0 
           utxos = []
@@ -199,7 +202,7 @@ def wallet(syncer, config):
                   continue
                 if isinstance(_list[address][texted_index], int):
                   _index = base64.b64decode(texted_index.encode())
-                  ser_priv, ser_blinding, apc = km.get_output_private_data(_index, r_txn = w_txn)
+                  ser_priv, ser_blinding, apc = km.get_output_private_data(_index, cursor)
                   priv = PrivateKey(ser_priv, raw=True)
                   blinding = PrivateKey(ser_blinding, raw=True)
                   utxos.append( (_index, _list[address][texted_index], priv, blinding, apc) )
@@ -217,8 +220,8 @@ def wallet(syncer, config):
               continue
           tx = Transaction(None, None)
           tx.add_destination((a, value, True))
-          tx.blindly_generate(km.new_address(w_txn), utxos, config["fee_policy"].get("generate_fee_per_kb", 3000))        
-          km.save_generated_transaction(tx, w_txn)
+          tx.blindly_generate(km.new_address(cursor), utxos, config["fee_policy"].get("generate_fee_per_kb", 3000))        
+          km.save_generated_transaction(tx, cursor)
           response["result"]=tx.serialize()
           syncer.queues[message['sender']].put(response)
       if message['action']=="stop":
